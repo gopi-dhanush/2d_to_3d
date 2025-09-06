e78a87ee1e9b52f870eb1f9b65bc86d1c492ffda
gopi-dhanush <gopidhanush819@gmail.com>
2025-09-06 15:12:42 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+
+import random
+a=random.randint(0,3)
+print("computer no:",a)
+#
+import random
+i=random.randint(0,3)
+print("your no is:",i)
+#
+if a==i:
+    print("can ")
+else:
+    print("can't")
